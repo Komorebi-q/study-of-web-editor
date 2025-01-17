e13d2822343eb6d8d6565a78c6c6034d272aaea3
komorebi_qkc <komorebi_qkc@163.com>
2025-01-17 08:39:24 +0800

    expr s0.1

--- komo-editor/src/model/__test__/content.test.ts
+++ komo-editor/src/model/__test__/content.test.ts
@@ -1,7 +1,37 @@
 import { test, expect } from "vitest";
+import { TokenString } from "../content";
 
-
-test("content TokenString", () => {})
+test("content TokenString", () => {
+  const tokenString = new TokenString(
+    "heading{1,3} (text | image)+ paragraph?",
+    { a: { type: "a" } }
+  );
+  const res = [
+    "heading",
+    "{",
+    "1",
+    ",",
+    "3",
+    "}",
+    "(",
+    "text",
+    "|",
+    "image",
+    ")",
+    "+",
+    "paragraph",
+    "?"
+  ];
+  for (const item of res) {
+    expect(tokenString.next).toBe(item);
+    const ret = tokenString.eat(item);
+    if (typeof ret === "boolean") {
+      expect(ret).toBe(true);
+    } else {
+      expect(ret).toBe(tokenString.pos-1);
+    }
+  }
+});
 test("content expression", () => {
   expect(1 + 1).toBe(2);
 });
--- komo-editor/src/model/content.ts
+++ komo-editor/src/model/content.ts
@@ -12,16 +12,16 @@
 type NodeType = { type: string };
 
 type Expr =
-  | { type: "choice"; exprs: Expr[] }
-  | { type: "seq"; exprs: Expr[] }
-  | { type: "plus"; exprs: Expr[] }
-  | { type: "star"; exprs: Expr[] }
-  | { type: "opt"; exprs: Expr[] }
-  | { type: "range"; exprs: Expr[] }
+  | { type: "choice"; expr: Expr[] }
+  | { type: "seq"; expr: Expr[] }
+  | { type: "plus"; expr: Expr[] }
+  | { type: "star"; expr: Expr[] }
+  | { type: "opt"; expr: Expr[] }
+  | { type: "range"; expr: Expr[] }
   // TODO   value should be NodeType
   | { type: "name"; value: any };
 
-class TokenString {
+export class TokenStream {
   inline: boolean | null = null;
   pos: number = 0;
   tokens: string[] = [];
@@ -30,7 +30,8 @@ class TokenString {
     readonly string: string,
     readonly nodeTypes: { readonly [key: string]: NodeType }
   ) {
-    this.tokens = string.split("s*(=?\b|W|$)");
+    this.tokens = string.split(/\s*(?=\b|\W|$)/);
+    console.log("tokens", this.tokens);
     if (this.tokens[0] === "") {
       this.tokens.shift();
     }
@@ -51,5 +52,90 @@ class TokenString {
     throw new SyntaxError(
       str + " (in content expression '" + this.string + "')"
     );
+  }
+}
+
+function parseExpr(stream: TokenStream): Expr {
+  const expr: Expr[] = [];
+  do {
+    expr.push(parseExprSeq(stream));
+  } while (stream.eat("|"));
+
+  return expr.length === 1 ? expr[0] : { type: "choice", expr };
+}
+
+function parseExprSeq(stream: TokenStream): Expr {
+  const expr: Expr[] = [];
+  do {
+    expr.push(parseExprSubscript(stream));
+  } while (stream.next && stream.next !== ")" && stream.next !== "|");
+  return expr.length === 1 ? expr[0] : { type: "seq", expr };
+}
+
+function parseExprSubscript(stream: TokenStream): Expr {
+  let expr = parseExprAtom(stream);
+
+  for (;;) {
+    if (stream.eat("+")) {
+      return {
+        type: "plus",
+        expr
+      };
+    } else if (stream.eat("*")) {
+      return {
+        type: "star",
+        expr
+      };
+    } else if (stream.eat("?")) {
+      return {
+        type: "opt",
+        expr
+      };
+    } else if (stream.next === "{") {
+      return parseExprRange(stream, expr);
+    } else {
+      break;
+    }
+  }
+
+  return expr;
+}
+
+function resolveName(stream: TokenStream, name: string): readonly NodeType[] {
+  let types = stream.nodeTypes, type = types[name]
+  if (type) return [type]
+  let result: NodeType[] = []
+  for (let typeName in types) {
+    let type = types[typeName]
+    if (type.isInGroup(name)) result.push(type)
+  }
+  if (result.length == 0) stream.err("No node type or group '" + name + "' found")
+  return result
+}
+
+function parseExprAtom(stream: TokenStream): Expr {
+  if (stream.eat("(")) {
+    const expr = parseExpr(stream);
+    if (!stream.eat(")")) {
+      stream.err("Unmatched ')'");
+    }
+
+    return expr;
+  } else if (!/\W/.test(stream.next)) {
+    let exprs = resolveName(stream, stream.next).map((type) => {
+      return {
+        type: "name",
+        value: type
+      } as Expr;
+    });
+    stream.pos++;
+    return exprs.length === 1
+      ? exprs[0]
+      : {
+          type: "choice",
+          expr: exprs
+        };
+  } else {
+    stream.err("Unexpected token '" + stream.next + "'");
   }
 }
